93b6546c3512dcbd698aa043b7c6faf2945c7449
Anish Shukla <shuklaanish040@gmail.com>
2025-02-27 20:30:34 +0530

    movie

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,20 @@ import React from "react";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-red-600 hover:text-yellow-200 cursor-pointer underline">
-        Anish shukla
-      </h1>
-    </div>
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./assets/hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span>You'll Enjoy
+            Without the Hassle
+          </h1>
+        </header>
+        <p>Search</p>
+      </div>
+    </main>
   );
 };
 
