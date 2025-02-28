b5db5e8800ef4a0bb5a65ee064b7087bdd01892e
Anish Shukla <shuklaanish040@gmail.com>
2025-02-28 12:16:06 +0530

    movie

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
 import React, { useState } from "react";
 import Search from "./components/Search";
 
+const API_BASE_URL = "https://api.themoviedb.org/3";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
+
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   return (
